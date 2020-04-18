1a271311e10bd01ec16b2ae1598a4a5a911626e8
jack <roxmagis@ymail.com>
2020-04-18 20:32:58 +0700

    Fixed super() error and include header and footer components in container

--- src/components/container.js
+++ src/components/container.js
@@ -2,15 +2,17 @@ import React from 'react';
 import Header from './header.js';
 import Footer from './footer.js';
 
-class Container extends React.Components {
-	
+class Container extends React.Component {
 	constructor(prop){
 		super(prop);
 	}
 
 	render(){
 		return(
-			<div className="Container"></div>
+			<div className="Container">
+                <Header/>
+                <Footer/>
+            </div>
 		);
 	}
 }
