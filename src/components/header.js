b9a4a5a8df7e370680680f24d9a97312b6300dd9
knobhead <trustz01@gmail.com>
2020-04-18 10:55:28 +0700

    added class Header
    still not working

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,13 @@ import React from 'react';
 class Header extends React.Component {
 	render(){
 		return(
-			
+			<div className="Header">
+				<Header>
+					<h1> Vafflehauz </h1>
+				</Header>
+			</div>
 		);
 	}
 }
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/header.js';
+import './components/style.css';
