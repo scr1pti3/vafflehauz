6dbaae8c2615212d52eab5c06dabcf08c4826d79
knobhead <trustz01@gmail.com>
2020-04-20 18:20:20 +0700

    added menu.js
    still not working, Cannot read property 'Item' of undefined and ./src/components/menu.js export 'default' (imported as 'Nav') was not found in 'react-bootstrap'

--- src/components/container.js
+++ src/components/container.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './header.js';
 import Footer from './footer.js';
+import Menu from './menu.js';
 
 class Container extends React.Component {
 
@@ -13,6 +14,7 @@ class Container extends React.Component {
 			<div className="Container">
                 <Header/>
                 <Footer/>
+                <Menu/>
             </div>
 		);
 	}
---
+++ src/components/menu.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Nav from 'react-bootstrap';
+
+class Menu extends React.Component {
+
+	constructor(prop){
+		super(prop);
+	}
+
+	render (){
+		return (
+			<div className="Menu">
+				<Nav fill variant="tabs" className="justify-content-end" activeKey="">
+   			    	<Nav.Item>
+     					<Nav.Link href="link-1">Link 1</Nav.Link>
+   					</Nav.Item>
+
+   					<Nav.Item>
+      					<Nav.Link eventKey="link-2">Link 2</Nav.Link>
+    				</Nav.Item>
+
+    				<Nav.Item>
+      					<Nav.Link eventKey="link-3">Link 3</Nav.Link>
+   					</Nav.Item>
+  				</Nav>
+			</div>
+		
+		);
+	}
+}
+
+export default Menu;
