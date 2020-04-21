b2aab594f95ad77adf706f23f0050e015aa4a27d
jack <roxmagis@ymail.com>
2020-04-21 16:56:05 +0700

    Nav Links are now clickable in NavBar Components

--- src/components/header/navbar.js
+++ src/components/header/navbar.js
@@ -14,23 +14,27 @@ const brandStyle = {
 }
 
 function NavBar (props) {
-    const [isActive, setActive] = useState({
-        home: false,
-        gallery: false,
-        about: false
-    });
+    const [isActive, setActive] = useState({Home: true});
+
+    function toggle (e) {
+        let key = e.target.text;
+        let temp = {};
+        temp[key] = true;
+        setActive(temp);
+    }
+
     return (
         <Navbar light color="light" expand="lg" className="justify-content-between">
                 <NavbarBrand href="#home" style={brandStyle}>Vafflehauz</NavbarBrand>
                 <Nav pills>
                     <NavItem>
-                        <NavLink href="#home" active={isActive.home}>Home</NavLink>
+                        <NavLink href="#home" active={isActive.Home} onClick={(e) => toggle(e)}>Home</NavLink>
                     </NavItem>
                     <NavItem>
-                        <NavLink href="#gallery" active={isActive.gallery}>Gallery</NavLink>
+                        <NavLink href="#gallery" active={isActive.Gallery} onClick={(e) => toggle(e)}>Gallery</NavLink>
                     </NavItem>
                     <NavItem>
-                        <NavLink href="#about" active={isActive.about}>About</NavLink>
+                        <NavLink href="#about" active={isActive.About} onClick={(e) => toggle(e)}>About</NavLink>
                     </NavItem>
                 </Nav>
         </Navbar>
