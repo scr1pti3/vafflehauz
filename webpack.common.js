7d85203b1bee90e3754e2533c0dcfec446861fa5
Jack <muhammadraznan@satpaminternet.com>
2020-05-21 00:21:15 +0700

    split vendor as individual npm module chunk

--- webpack.common.js
+++ webpack.common.js
@@ -18,11 +18,16 @@ const optimization = {
     runtimeChunk: 'single',
     usedExports: true,
     splitChunks: {
+        chunks: 'all',
+        maxInitialRequests: Infinity,
+        minSize: 0,
         cacheGroups: {
             vendor: {
                 test: /[\\/]node_modules[\\/]/,
-                name: 'vendors',
-                chunks: 'all'
+                name(module) {
+                  const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+                  return `npm.${packageName.replace('@', '')}`;
+                }
             }
         }
     }
